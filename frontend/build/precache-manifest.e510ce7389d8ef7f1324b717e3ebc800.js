c15906c22aacfacdf48489b34ecdcfb5db8c1ec9
leePersephone <djebaili4282@gmail.com>
2023-02-01 01:54:59 +0100

    change 3

---
+++ frontend/build/precache-manifest.e510ce7389d8ef7f1324b717e3ebc800.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b9d5e4378ad84b87a617bf61b3a9a9",
+    "url": "/SA/index.html"
+  },
+  {
+    "revision": "f03458bd5ab297d13238",
+    "url": "/SA/static/css/main.a42bca8e.chunk.css"
+  },
+  {
+    "revision": "c3604257b98e5eff2c02",
+    "url": "/SA/static/js/2.94b97214.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/SA/static/js/2.94b97214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03458bd5ab297d13238",
+    "url": "/SA/static/js/main.721b864e.chunk.js"
+  },
+  {
+    "revision": "8237f546ac0abf95c4a9",
+    "url": "/SA/static/js/runtime-main.7c8f8297.js"
+  },
+  {
+    "revision": "de0a5a2b2355c9a622590be753cab1b4",
+    "url": "/SA/static/media/city.de0a5a2b.png"
+  }
+]);
